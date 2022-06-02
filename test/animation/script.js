f71cd5bebe67740c1279498d23257a7ab5b5f64e
totonmay <103137529+totonmay@users.noreply.github.com>
2022-06-02 11:47:32 -0700

    animation

---
+++ test/animation/script.js
@@ -0,0 +1,7 @@
+//scroll animation//
+$(function() {
+    $('.scroll-down').click (function() {
+      $('html, body').animate({scrollTop: $('section.ok').offset().top }, 'slow');
+      return false;
+    });
+  });
